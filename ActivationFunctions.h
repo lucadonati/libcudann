3eab6a407c7b8367012c802fd76fb17bd894abcc
lucadonati <lucadonati85@gmail.com>
2017-07-20 09:59:50 +0200

    a

--- ActivationFunctions.h
+++ ActivationFunctions.h
@@ -12,7 +12,11 @@ const int ACT_SIGMOID = 1;
 const int ACT_TANH    = 2;
 const int ACT_RELU    = 3;
 
+
 template <typename T1, typename T2, typename T3>
+#ifdef __CUDACC__
+__host__ __device__
+#endif
 inline T1 clip(T1 && x, T2 && lo, T3 && hi) {
     return (x < lo) ? lo : ((x > hi) ? (hi) : (x));
 }
@@ -39,14 +43,14 @@ inline float actDerivation(int act, float y) {
 }
 /// returns the span size of a function for error calculation
 /// just used for last layer??
+#ifdef __CUDACC__
+__host__ __device__
+#endif
 inline float spanSize(int act) {
-    switch (act) {
-        case ACT_LINEAR:
-        case ACT_SIGMOID:   return 1.0f;
-        case ACT_TANH:      return 2.0f;
-        case ACT_RELU:      return 1.0f;
-        default:            throw std::runtime_error("Function not yet implemented");
-    }
+    if (act == ACT_TANH)
+        return 2.0f;
+    else
+        return 1.0f;
 }
 
 
